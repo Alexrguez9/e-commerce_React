210cf71fbcbc9446698b751f6bf59a40de80a75b
Alexrguez9 <72986293+Alexrguez9@users.noreply.github.com>
2024-02-16 17:04:38 +0100

    US-7. Restringir información a usuarios no autenticados
    Created ProtectedRoute.jsx and used it to protect cart page and productDetails page

--- myshop/src/router/index.jsx
+++ myshop/src/router/index.jsx
@@ -5,6 +5,7 @@ import Cart from '../components/Cart';
 import Profile from '../views/Profile';
 import NotFound from '../views/NotFound';
 import ProductDetails from '../views/ProductDetails';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 
 export const router = createBrowserRouter([
@@ -13,8 +14,8 @@ export const router = createBrowserRouter([
         element: <Layout />,
         children: [
             { path: '/', element: <Products /> },
-            { path: '/product/:productId', element: <ProductDetails /> },
-            { path: '/cart', element: <Cart /> },
+            { path: '/product/:productId', element: <ProtectedRoute><ProductDetails /> </ProtectedRoute>},
+            { path: '/cart', element: <ProtectedRoute><Cart /></ProtectedRoute> },
             { path: '/login', element: <Profile />},
             { path: '/*', element: <NotFound />}
         ]
