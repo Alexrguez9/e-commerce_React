fd3344fc3b04b2e58dbdf12e9363f6739a5180e6
Alexrguez9 <72986293+Alexrguez9@users.noreply.github.com>
2024-03-11 18:16:33 +0100

    Created New Product form with react-hook-forms

--- myshop/src/components/ProductEditModal.jsx
+++ myshop/src/components/ProductEditModal.jsx
@@ -7,9 +7,34 @@ import {
     updateProductThunk,
     getAllProductsThunk,
 } from '../redux/reducers/productReducer';
+import { useForm } from 'react-hook-form';
 
 const ProductEditModal = ({ product, closeModal }) => {
     const [editedFields, setEditedFields] = useState({
+        title: product.title,
+        price: product.price,
+        description: product.description,
+        image: product.image
+    });
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        clearErrors,
+        setError,
+        formState: { errors },
+        reset,
+    } = useForm({
+        defaultValues: {
+            title: product.title,
+            price: product.price,
+            description: product.description,
+            image: product.image
+        }
+    });
+
+    reset({
         title: product.title,
         price: product.price,
         description: product.description,
@@ -28,12 +53,12 @@ const ProductEditModal = ({ product, closeModal }) => {
         }));
     };
 
-    const handleSubmitForm = async (e) => {
-        e.preventDefault();
+    const handleSubmitForm = async (data) => {
+
 
         const updatedProduct = {
             ...product,
-            ...editedFields
+            ...data
         };
 
         dispatch(updateProductThunk(updatedProduct));
@@ -53,30 +78,60 @@ const ProductEditModal = ({ product, closeModal }) => {
                     onSubmit={handleSubmitForm}
                     className="form-modal-container"
                 >
-                    <label htmlFor="title">Title</label>
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        value={editedFields.title}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="price">Price</label>
-                    <input
-                        type="text"
-                        id="price"
-                        name="price"
-                        value={editedFields.price}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                        type="text"
-                        id="description"
-                        name="description"
-                        value={editedFields.description}
-                        onChange={handleInputChange}
-                    />
+                    <div className="form-field">
+                        <label>
+                            Title
+                            <input
+                                type="text"
+                                {...register("title", { 
+                                    required: "Por favor, introduce un titulo",
+                                    minLength: {
+                                        value: 3,
+                                        message: "El nombre debe tener al menos 3 caracteres"
+                                    },
+                                    maxLength: {
+                                        value: 100,
+                                        message: "El nombre debe tener menos de 40 caracteres"
+                                    }
+                                })}
+                            />
+                            {errors.title && <span>{errors.title.message}</span>}
+                        </label>
+                    </div>
+                    <div className="form-field">
+                        <label>Price</label>
+                        <input
+                            type="text"
+                            {...register("price", {
+                                required: "Por favor, introduce el precio",
+                                min: {
+                                    value: 0.01,
+                                    message: "El precio debe ser mayor que 0"
+                                },
+                            })}
+                        />
+                        {errors.price && <span>{errors.price.message}</span>}
+                    </div>
+                    <div className="form-field">
+                        <label>
+                            Description
+                            <textarea
+                                type="text"
+                                {...register("description", { 
+                                    required: "Por favor, introduce una descripción.",
+                                    minLength: {
+                                        value: 3,
+                                        message: "La descripción debe tener al menos 3 caracteres"
+                                    },
+                                    maxLength: {
+                                        value: 100,
+                                        message: "La descripción debe tener menos de 100 caracteres"
+                                    }
+                                })}
+                            />
+                            {errors.description && <span>{errors.description.message}</span>}
+                        </label>
+                    </div>
                     <button type="submit">Guardar</button>
                 </form>
             </div>
